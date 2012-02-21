fc640fbfdce3b9d5ac19c45429602cc46271d029
Byron Walker <byron.walker@angryagile.com.au>
2012-02-21 15:26:44 +1100

    pairing

---
+++ Tests/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tests {
+    
+    [TestFixture]
+    public class TestClass {
+        
+        [Test]
+        public void should_be_0_if_empty_string_inputted()
+        {
+            var input = "";
+            var sum = Add(input);
+
+            Assert.That(sum, Is.EqualTo(0));
+        }
+
+        private int Add(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
